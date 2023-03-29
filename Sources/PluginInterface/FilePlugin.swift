e4e3dab9329a56a518d9961ca6af9abc091644e1
Ilya Knyazev <knyazevilya03@gmail.com>
2023-03-29 23:40:33 +0300

    test 1

--- Sources/PluginInterface/FilePlugin.swift
+++ Sources/PluginInterface/FilePlugin.swift
@@ -10,3 +10,11 @@ import Foundation
 public protocol FilePlugin {
     var supportedFileType: String { get }
 }
+
+open class PluginBuilder<T: FilePlugin> {
+    public init() {}
+    
+    open func build() -> T {
+        preconditionFailure("Method must be implemented")
+    }
+}
